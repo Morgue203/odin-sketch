24d6c071e94e9c6988a1912fac86983177a21419
Dominic <xuanvinguyen2003@gmail.com>
2024-03-14 14:38:44 +0700

    initial setup for css file & todo list
    added style.css file for flexbox and grid styling, also created a todo list in js file for future guidance

---
+++ script.js
@@ -0,0 +1,4 @@
+//TODO
+//1. create a 16x16 with javascript & style with flexbox
+//2. find out how to set up a hover effect for the div grid
+//3. add a button that reset current grid and allows user to enter the size for the new grid
