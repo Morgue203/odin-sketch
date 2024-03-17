d3b856438193546532474b42d6f4348c14ab6d0d
Dominic <xuanvinguyen2003@gmail.com>
2024-03-17 21:18:03 +0700

    grid function, grid styling finished the createGrid function along with some basic styling for gridCells, slight alternation in html file

--- script.js
+++ script.js
@@ -2,3 +2,29 @@
 //1. create a 16x16 with javascript & style with flexbox
 //2. find out how to set up a hover effect for the div grid
 //3. add a button that reset current grid and allows user to enter the size for the new grid
+
+
+//set up constants 
+const GRIDSIDE = 700;
+let rows = 16;
+let cols = 16;
+
+//container sizing
+const container = document.querySelector(".container");
+container.style.width = GRIDSIDE + 'px';
+container.style.height = GRIDSIDE + 'px';
+
+//function to create a grid 
+function createGrid() {
+    for (let i = 0; i < (rows * cols); i++) {
+        const gridCell = document.createElement("div");
+        //cell sizing
+        gridCell.style.width = (GRIDSIDE / cols) - 2 + 'px';
+        gridCell.style.height = (GRIDSIDE / rows) - 2 + 'px';
+        //add a class name cell for each gridCell
+        gridCell.classList.add("cell");
+        container.appendChild(gridCell);
+    }
+}
+
+createGrid();
